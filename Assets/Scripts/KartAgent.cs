1a93b70397045ab3ed1d31887ad322bb7851aa27
canuski <125011800+canuski@users.noreply.github.com>
2024-04-30 14:44:17 +0200

    Add Assets

---
+++ Assets/Scripts/KartAgent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class KartAgent : Agent
+{
+   public CheckpointManager _checkpointManager;
+   private KartController _kartController;
+   
+   //called once at the start
+   public override void Initialize()
+   {
+      _kartController = GetComponent<KartController>();
+   }
+   
+   //Called each time it has timed-out or has reached the goal
+   public override void OnEpisodeBegin()
+   {
+      _checkpointManager.ResetCheckpoints();
+      _kartController.Respawn();
+   }
+
+   #region Edit this region!
+
+      //Collecting extra Information that isn't picked up by the RaycastSensors
+      public override void CollectObservations(VectorSensor sensor)
+      {
+         
+      }
+
+      //Processing the actions received
+      public override void OnActionReceived(ActionBuffers actions)
+      {
+         
+      }
+      
+      //For manual testing with human input, the actionsOut defined here will be sent to OnActionRecieved
+      public override void Heuristic(in ActionBuffers actionsOut)
+      {
+         
+      }
+      
+   #endregion
+}
