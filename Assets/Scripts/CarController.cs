f9cfc6f318d605e5ad8f9bac0796a55f4b3950bc
canuski <125011800+canuski@users.noreply.github.com>
2024-05-05 15:08:58 +0200

    First working version VR Drive

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,18 +1,33 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarCOntroller : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private PlayerInput inputManager;
+    public List<WheelCollider> throttleWheels;
+    public List<WheelCollider> steeringWheels;
+    public float strengthCoefficient = 200000f;
+    public float maxTurn = 20f;
+
     void Start()
     {
-        
+        inputManager = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        foreach (WheelCollider wheel in throttleWheels)
+        {
+            wheel.motorTorque = strengthCoefficient * Time.deltaTime * inputManager.Acceleration * 1000;
+            wheel.wheelDampingRate = inputManager.wheelDampening;
+        }
+
+        foreach (WheelCollider wheel in steeringWheels)
+        {
+            wheel.steerAngle = maxTurn * inputManager.Steering;
+            wheel.wheelDampingRate *= inputManager.wheelDampening;
+        }
     }
 }
