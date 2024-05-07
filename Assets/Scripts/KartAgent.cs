1f90ef7bf610464e6401ea989a46ba8cab7af1fb
Laurence Leysen TM <laurenceleysen5@gmail.com>
2024-05-07 14:25:53 +0200

    Rays/ ML setup

--- Assets/Scripts/KartAgent.cs
+++ Assets/Scripts/KartAgent.cs
@@ -23,24 +23,33 @@ public class KartAgent : Agent
       _kartController.Respawn();
    }
 
-   #region Edit this region!
+    #region Edit this region!
 
-      //Collecting extra Information that isn't picked up by the RaycastSensors
-      public override void CollectObservations(VectorSensor sensor)
-      {
-         
-      }
+    //Collecting extra Information that isn't picked up by the RaycastSensors
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        AddReward(-0.0001f);
+    }
 
-      //Processing the actions received
-      public override void OnActionReceived(ActionBuffers actions)
+    //Processing the actions received
+    public override void OnActionReceived(ActionBuffers actions)
       {
-         
+         var input = actions.ContinuousActions;
+
+        _kartController.ApplyAcceleration(input[1]);
+
+        _kartController.Steer(input[0]);
       }
       
       //For manual testing with human input, the actionsOut defined here will be sent to OnActionRecieved
       public override void Heuristic(in ActionBuffers actionsOut)
       {
-         
+        var action = actionsOut.ContinuousActions;
+        action[0] = Input.GetAxis("Horizontal"); //steering
+
+        action[1] = Input.GetKey(KeyCode.W) ? 1f : 0f; //Acceleration
+
+
       }
       
    #endregion
